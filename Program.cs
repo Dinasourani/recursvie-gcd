e6250e9a40bafc67d8777beb6e2511a5ef19aa21
Dina sourani <dyxz8613@gmail.com>
2025-12-12 13:44:53 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static int maghsom(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            return maghsom(b, a % b);
+        }
+        static void Main(string[] args)
+        {
+             Console.Write("عدد اول را وارد کنید: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("عدد دوم را وارد کنید: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int result = maghsom(num1, num2);
+
+        Console.WriteLine("بزرگ‌ترین مقسوم‌علیه مشترک  برابر است با:", result);
+        Console.ReadKey();
+        }
+        
+    }
+}
